230f3990ef473b49fa03c82a7296dc01eeda604a
jan-abt <jan.abt@gmail.com>
2025-07-19 16:57:06 +0700

    Sync UI with backend (bring in IDL ...)

--- app/page.js
+++ app/page.js
@@ -16,18 +16,22 @@ import { AnchorProvider, Program } from "@coral-xyz/anchor"
 import Header from "./components/Header"
 
 // Import config & IDL
-// import config from "@/app/config.json"
-// import Crowdsale from "@/app/idl/crowdsale.json"
+import config from "./config.json"
+import Crowdsale from "./idl/crowdsale.json"
+import { CONFIG_FILES } from "next/dist/shared/lib/constants"
 
 
 
 export default function Home() {
-
   // state manaement
   const [provider, setProvider] = useState(null)
   const [anchorProvider, setAnchorProvider] = useState(null)
   const [user, setUser] = useState(null)
   const [userBalance, setUserBalance] = useState(0)
+  const [userTokenBalance, setUserTokenBalance] = useState(0)
+  const [crowdsaleProgram, setCrowdsaleProgram] = useState(null)
+  const [crowdsaleBalance, setCrowdsaleBalance] = useState(0)
+  const [crowdsaleCost, setCrowdsaleCost] = useState(0)
 
   const getProvider = async () => {
     if ("phantom" in window) { // phantom installed?
@@ -46,11 +50,18 @@ export default function Home() {
           setUser(pubKey)
 
           // Get Crowdsale program
+          const crowdsaleProgram = new Program(Crowdsale, anchorProvider)
+          setCrowdsaleProgram(crowdsaleProgram)
 
           // Fetch Crowdsale state
+          const crowsaleState = await crowdsaleProgram.account.crowdsale.fetch(config.CROWDSALE_PDA)
+          setCrowdsaleCost(crowsaleState.cost)
 
           // Fetch Balance
           await getUserBalance(anchorProvider)
+          await getCrowdsaleBalance(anchorProvider)
+
+         // await getCrowdsaleBalance(anchorProvider)
         })
         // disconnect from phantom wallet
         provider.on("disconnect", () => {
@@ -63,9 +74,34 @@ export default function Home() {
   }
 
   const getUserBalance = async (anchorProvider) => {
+
     const userPubKey = new PublicKey(anchorProvider.wallet)
+    const tokenPubKey = new PublicKey(config.TOKEN_MINT_ACCOUNT)
     const userBalance = await anchorProvider.connection.getBalance(userPubKey)
     setUserBalance(userBalance)
+
+    const userTokenAccount = getAssociatedTokenAddressSync(tokenPubKey, userPubKey, true)
+    const userTokenAccountInfo = await anchorProvider.connection.getAccountInfo(userTokenAccount)
+    
+    if(userTokenAccountInfo){
+      const userTokenBalance = await anchorProvider.connection.getTokenAccountBalance(userTokenAccount)
+      setUserTokenBalance(userTokenBalance.value.account)
+    }
+
+  }
+
+  const getCrowdsaleBalance = async (anchorProvider) => {
+
+    const crowdsalePDAKey = new PublicKey(config.CROWDSALE_PDA)
+    const crowdsalePDATokenKey = new PublicKey(config.CROWDSALE_PDA_TOKEN_ACCOUNT)
+    
+    const crowdsaleBalance = await anchorProvider.connection.getBalance(crowdsalePDAKey)
+    setCrowdsaleBalance(crowdsaleBalance)
+
+    const crowdsaleTokenBalance = await anchorProvider.connection.getBalance(crowdsalePDATokenKey)
+    setCrowdsaleBalance(crowdsaleTokenBalance)
+
+
   }
 
   useEffect(() => {
